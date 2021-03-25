3ca10a3708deb5592f759e167888e9643716e61b
GuanRongFeng <rongfeng@Yaneguan.com>
2021-03-25 09:52:47 +0800

    first qtcreate

--- test3/widget.cpp
+++ test3/widget.cpp
@@ -1,12 +1,12 @@
 #include "widget.h"
 #include "ui_widget.h"
-
+#include <QDebug>
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
-
+    qDebug()<<"hello world";
 }
 
 Widget::~Widget()
